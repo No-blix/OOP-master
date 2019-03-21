d13e79ab9dcb81029e460b92d4e3ab15fa35e6e0
Oliver Lock <43725943+No-blix@users.noreply.github.com>
2019-03-21 18:00:02 +0100

    Nytt prosjekt

--- ValutaCalculator/src/sample/DataHandler/DataHandler.java
+++ ValutaCalculator/src/sample/DataHandler/DataHandler.java
@@ -1,34 +1,48 @@
 package sample.DataHandler;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import org.w3c.dom.css.CSSValueList;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class DataHandler {
 
-    public void getFile(){
-        GenerateReader();
 
-        return GenerateReader();
+    private final static ObservableList<String> listReader = FXCollections.observableArrayList();
+
+    public static ObservableList<String> getListReader(){
+        if(listReader.isEmpty()){
+            GenerateReader();
+        }
+        return listReader;
     }
 
 
 
+
+
+
     public static void GenerateReader(){
-        String csv = "valutakurser.csv";
-        File file = new File(csv);
-
+        String fileLocation = "../DataHandler/valutakurser.csv";
+        String inputLine = "";
         try {
-            Scanner scanner = new Scanner(file);
-            while(scanner.hasNext()){
-                String data = scanner.next();
+           Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileLocation)));
 
-                System.out.println(data);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+           while (scanner.hasNext()){
+               String data = scanner.next();
+
+               System.out.println(data);
+           }
+
+
+            } catch (IOException e1) {
+            e1.printStackTrace();
         }
+
     }
 
 }
